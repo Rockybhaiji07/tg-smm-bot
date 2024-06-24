a084638d4f125203df93ee9572a4b2b9a5829831
Rockybhaiji07 <86160003+Rockybhaiji07@users.noreply.github.com>
2024-06-24 06:34:12 +0530

    Update smm.py

--- smm.py
+++ smm.py
@@ -4,17 +4,17 @@ from telegram.error import TelegramError
 # Replace 'YOUR_BOT_TOKEN' with the token provided by BotFather
 bot_token = 'YOUR_BOT_TOKEN'
 
-def join_group_or_channel(chat_id):
+def join_group_or_channel(-1001555092484):
     try:
-        bot = Bot(token=bot_token)
-        bot.join_chat(chat_id)
-        print(f"Successfully joined the group/channel with ID: {chat_id}")
+        bot = Bot(token=6958175960:AAFbsEQ19lATd6NUCOhyhyi6ikSkoFvOfGk)
+        bot.join_chat(@ownerRocky)
+        print(f"Successfully joined the group/channel with ID: {@OwnerRocky}")
     except TelegramError as e:
         print(f"Failed to join the group/channel: {e}")
 
 if __name__ == "__main__":
     # Replace 'GROUP_OR_CHANNEL_ID' with the ID of the group or channel you want to join
-    group_or_channel_id = 'GROUP_OR_CHANNEL_ID'
-    join_group_or_channel(group_or_channel_id)
+    group_or_channel_id = '-1001555092484'
+    join_group_or_channel(-1001555092484)
 
 //tool made my codeprofessor on telegram
